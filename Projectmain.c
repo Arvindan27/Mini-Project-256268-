90342c86a653ccdad6a57b87b22af00bc8b74ee2
Arvindan27 <arvindan.p.27@gmail.com>
2021-04-13 21:47:23 +0530

    signup/login

--- Projectmain.c
+++ Projectmain.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 void signup(void);
+void login(void);
 int main(){
     int l;
     printf("WELCOME to the Ride sharing application\n");
@@ -7,6 +8,7 @@ int main(){
     scanf("%d",&l);
     if(l==1){
         printf("welcome new user\n");
+        login();
     }else{
         printf("welcomeback\n");
         signup();
@@ -14,8 +16,40 @@ int main(){
 }
 void signup(void){
     char email[39];
+    char pass[20];
+    char rpass[20];
     printf("Enter Your  email id\n");
     scanf("%39s",email);
-    printf("%39s",email);
+    printf("%39s\n",email);
+    printf("Enter Password\n");
+    scanf("%20s",pass);
+    printf("re enter password\n",rpass);
+    scanf("%20s",rpass);
+    if(strcmp(pass,rpass)==0){
+        printf("success\n");
+        login();
+    }else{
+        printf("something wrong\n");
+        
+    }
+
+}
+void login(void){
+    char email[39];
+    char pass[20];
+    int n;
+    printf("Enter Your  email id\n");
+    scanf("%39s",email);
+    printf("%39s\n",email);
+    printf("Enter Password\n");
+    scanf("%20s",pass);
+    printf("select between driver(1) or passanger(2)?\n");
+    scanf("%d",&n);
+    if(n==1){
+        printf("driver");
+    }else{
+        printf("passenger");
+    }
+
 
 }
