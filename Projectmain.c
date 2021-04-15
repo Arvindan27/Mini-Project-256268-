f7dcaf692a492d0312e3dee80f2ffdeb7650f417
Arvindan27 <arvindan.p.27@gmail.com>
2021-04-15 17:59:01 +0530

    makefile

--- Projectmain.c
+++ Projectmain.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include "functions.h"
+#include<string.h>
+#include<stdlib.h>
 void signup(void);
 void login(void);
 void driver(void);
@@ -26,7 +28,7 @@ void signup(void){
     printf("%39s\n",email);
     printf("Enter Password\n");
     scanf("%20s",pass);
-    printf("re enter password\n",rpass);
+    printf("re enter password\n");
     scanf("%20s",rpass);
     if(strcmp(pass,rpass)==0){
         printf("success\n");
